caf3e5c89f8f5d81683bce3ce19cd0fdf30fca66
Annie Stewart <anniestewart@Annies-MacBook-Air.local>
2019-05-09 15:13:56 -0400

    heroku

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,4 +5,14 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+
+    var userInput = req.body;
+
+	var userAnswers = userInput.scores;
+	
+
+  });
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var htmlRoutes = require("./html-routes.js");
 
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 // load routes into app by passing app into route functions
 htmlRoutes(app);
